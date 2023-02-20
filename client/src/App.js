0286d469d26ebfa7d1e9321ef4e4dc0dfa3142a6
Max Herman <maxbherman12@gmail.com>
2023-02-20 16:40:09 -0600

    added sign in with google and axios basics

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,5 @@
+import React, {useEffect, useState} from 'react'
+
 import HomePage from "./pages/homepage/home.page";
 import Register from "./pages/register/register.page";
 import Login from "./pages/login/login.page";
@@ -6,14 +8,37 @@ import MyRecipesPage from "./pages/my-recipes/my-recipes.page";
 import AddRecipePage from './pages/add-recipe/add-recipe.page'
 import ProfilePage from './pages/profile/profile.page'
 import SettingsPage from './pages/settings/settings.page'
+import RecipePage from "./pages/recipe/recipe.page";
 
 import Layout from './components/layout/layout.component'
 import RequireAuth from "./components/RequireAuth";
 
 import {Routes, Route} from 'react-router-dom'
-import RecipePage from "./pages/recipe/recipe.page";
+import {api as axios} from "./utils/axios";
 
 function App(){
+    const [user, setUser]               = useState(null)
+
+    useEffect(() => {
+        async function auth(){
+            await axios({
+                method: "GET",
+                url: "/auth/user"
+            })
+            .then(resUser => setUser(resUser.data))
+            .catch(err => console.log("ERROR: ", err.json))
+    
+            // await axios({
+            //     method: "GET",
+            //     url: "/auth/exp"
+            // })
+            //     .then(res => setTimeout(() => setOpenDialog(true), res.data.exp*1000 - 10000)) //set dialog to pop up 10 seconds before token exp
+            //     .catch(err => console.log(err))
+        }
+
+        auth();
+    }, [])
+
     return(
         <Routes>
             <Route path="/" errorElement={<ErrorPage/>}> 
