f78305a87f2b6bcbeecc1e8668526e6692574902
Max Herman <maxbherman12@gmail.com>
2023-02-27 23:23:01 -0600

    created recipe-preview component and fixed sidebar

--- client/src/components/recipe-preview/recipe-preview.component.js
+++ client/src/components/recipe-preview/recipe-preview.component.js
@@ -1,22 +1,48 @@
 import React from 'react'
 import './recipe-preview.styles.css'
 
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+
 import {useNavigate} from 'react-router-dom'
 
 const RecipePreview = ({data}) => {
-    const {id, title, img} = data
+    const {id, title, img, desc} = data
     const navigate = useNavigate()
 
     return (
-        <div className="recipe-preview" onClick={() => navigate(`../recipe/${id}`)}>
-            <div className="img-cont">
-                <img src={img} />
-            </div>
-            <div className="text-cont">
-                <p>{title}</p>
-            </div>
-        </div>
+        <Grid item key={id} xs={12} sm={6} md={4}>
+            <Card
+                sx={{
+                    height: "50vh",
+                    display: "flex",
+                    flexDirection: "column",
+                    cursor: "pointer"
+                }}
+                onClick={() => navigate(`../recipe/${id}`)}
+            >
+                <CardMedia
+                    component="img"
+                    image={img}
+                    alt="recipe"
+                    style={{height: "60%"}}
+                />
+                <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography
+                        gutterBottom
+                        variant="h5"
+                        component="h2"
+                    >
+                        {title}
+                    </Typography>
+                    <Typography>{desc}</Typography>
+                </CardContent>
+            </Card>
+        </Grid>
     )
 }
-
+    
 export default RecipePreview
