81d836f5f0d6cf379ba24c35b7aa807e963b2b4d
David Atwood <70248461+atwooddc@users.noreply.github.com>
2023-04-05 10:11:33 -0500

    troubleshooting

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -35,11 +35,22 @@ router.get(
     }
 );
 
+
+// @route       GET auth/user
+// @desc        Get user data
+// @access      Private
+router.get('/user', (req, res) => {
+    User.findById(req.user._id)
+        .then(user => res.send(user))
+})
+
 // @route       GET auth/logout
 // @desc        Logout user
 // @access      Public
 router.get("/logout", function(req, res){
     res.clearCookie("token");
+    // req.session = null;
+    req.logout();
     res.redirect(`${getBaseUrl()}`);
 });
 
