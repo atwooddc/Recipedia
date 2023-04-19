200945a929af9f57f41d61054f096ec6c22ccca4
Max Herman <maxbherman12@gmail.com>
2023-04-19 13:04:57 -0500

    default to dashboard page on login + added sidebar button

--- client/src/pages/login/login.page.js
+++ client/src/pages/login/login.page.js
@@ -47,7 +47,7 @@ const Login = () => {
                     const user = data.user;
                     setAuth(user);
                     // redirect to protected route
-                    navigate("../myrecipes");
+                    navigate("../home");
                 })
                 .catch((error) => console.error(error));
         } catch (err) {
