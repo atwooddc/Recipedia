37c08ea43ba1644ce2e0603146962259c041a4db
David Atwood <70248461+atwooddc@users.noreply.github.com>
2023-04-19 01:22:34 -0500

    added  nonfunctional search bar - layout issues

--- client/src/pages/my-recipes/my-recipes.page.js
+++ client/src/pages/my-recipes/my-recipes.page.js
@@ -3,9 +3,13 @@ import "./my-recipes.styles.css";
 
 import RecipePreview from "../../components/recipe-preview/recipe-preview.component";
 import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
 import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Button from "@mui/material/Button";
+import SaveIcon from "@mui/icons-material/Save";
+import EditIcon from "@mui/icons-material/Edit";
 
 import useAuth from "../../hooks/useAuth";
 
@@ -41,43 +45,62 @@ const MyRecipesPage = () => {
     return (
         <div className="my-recipes-page">
             <h2>Hope you're hungry!</h2>
-            <ThemeProvider theme={theme}>
-                <Container
-                    sx={{ py: 8 }}
-                    maxWidth="md"
-                    style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                    }}
-                >
-                    <Grid container spacing={4}>
-                        <div
-                            className="btn-cont"
-                            style={{
-                                display: "flex",
-                                justifyContent: "flex-end",
-                                width: "100%",
+            <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+                <ThemeProvider theme={theme}>
+                    <Box
+                        sx={{
+                            mb: 1,
+                            flexDirection: "row",
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            maxWidth: "md",
+                        }}
+                    >
+                        <TextField
+                            name="query"
+                            id="query"
+                            label="Search"
+                            placeholder="Search your recipes!"
+                            // fullWidth
+                            autoFocus
+                            sx={{
+                                mt: 1,
+                                marginRight: "10px",
+                                height: "50px",
                             }}
+                        />
+                        <Button
+                            variant="contained"
+                            onClick={() => setEditMode(!editMode)}
+                            startIcon={editMode ? <SaveIcon /> : <EditIcon />}
+                            sx={{ minWidth: "100px", mt: 1, height: "50px" }}
                         >
-                            <Button
-                                variant="contained"
-                                onClick={() => setEditMode(!editMode)}
-                            >
-                                {editMode ? "Save" : "Edit"}
-                            </Button>
-                        </div>
-                        {auth.recipes?.map((recipe) => (
-                            <RecipePreview
-                                key={recipe._id}
-                                data={recipe}
-                                editMode={editMode}
-                                handleRemoveRecipe={handleRemoveRecipe}
-                            />
-                        ))}
-                    </Grid>
-                </Container>
-            </ThemeProvider>
+                            {editMode ? "Save" : "Edit"}
+                        </Button>
+                    </Box>
+                    <Container
+                        sx={{ py: 8 }}
+                        maxWidth="md"
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Grid container spacing={4}>
+                            {auth.recipes?.map((recipe) => (
+                                <RecipePreview
+                                    key={recipe._id}
+                                    data={recipe}
+                                    editMode={editMode}
+                                    handleRemoveRecipe={handleRemoveRecipe}
+                                />
+                            ))}
+                        </Grid>
+                    </Container>
+                </ThemeProvider>
+            </Container>
         </div>
     );
 };
