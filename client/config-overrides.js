850093cc170012245ebd6c7036669f2dfd2b2205
David Atwood <70248461+atwooddc@users.noreply.github.com>
2023-11-14 19:15:58 -0800

    rewired app to remedy client error messages

---
+++ client/config-overrides.js
@@ -0,0 +1,8 @@
+const webpack = require("webpack");
+module.exports = function override(config, env) {
+    //do stuff with the webpack config...
+
+    config.resolve.fallback = { crypto: false };
+
+    return config;
+};
